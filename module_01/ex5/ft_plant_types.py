c6eff3ba38d5d25833e043d1b46343d6c7b9c724
Gorka Gallardo Castany <gorkgall@cbr12s4.42barcelona.com>
2026-05-21 12:30:28 +0200

    Tree class added

--- module_01/ex5/ft_plant_types.py
+++ module_01/ex5/ft_plant_types.py
@@ -49,21 +49,45 @@ class Flower(Plant):
 
     def set_color(self, value: str) -> None:
         self._color = value
-
+    def	bloom(self)  -> None:
+        stats = super().get_stats()
+        print("[asking the rose to bloom]")
+        print(f"{stats}")
+        print(f" Color: {self._color}")
+        print(" Rose is blooming beautifully!")
     def print_stats(self) -> None:
         stats = super().get_stats()
         print("=== Flower")
         print(f"{stats}")
         print(f" Color: {self._color}")
         print(" Rose has not bloomed yet")
-        print("[asking the rose to bloom]")
-        print(f"{stats}")
-        print(f" Color: {self._color}")
-        print(" Rose is blooming beautifully!")
 
+class Tree(Plant):
+    def __init__(self, diameter:float, name:str, height:float, age:int) -> None:
+        super().__init__(name, height, age)
+        self._diameter = 0.0
+        self.set_diameter(diameter)
+    def get_diameter(self) -> float:
+        return self._diameter
+    def set_diameter(self, value:float) -> None:
+        if value < 0:
+            print("Error: Trunk diameter value can't be a negative number")
+        else:
+            self._diameter = value
+    def produce_shade(self) -> None:
+        print(f"[asking the {self._name} to produce shade]")
+        print(f"Tree {self._name} now produces a shade of {self._height}cm long and {self._diameter}cm wide.")
+    def print_stats(self) -> None:
+        stats = super().get_stats()
+        print(f"{stats}")
+        print(f"Trunk diameter: {self._diameter}")
 
 if __name__ == "__main__":
 
     print("=== Garden Plant Types ===")
     rose = Flower("red", "Rose", 15.0, 10)
+    oak = Tree(5.0, "Oak", 200.0, 365)
     rose.print_stats()
+    rose.bloom()
+    oak.print_stats()
+    oak.produce_shade()
